89fffa2b6ef55ef6f94436a0b04cbdbf48e72005
arunprasath786 <55042526+arunprasath786@users.noreply.github.com>
2019-09-08 17:41:38 +0530

    Update TC001_LoginLogout.java
    Latest testcase

--- LeafBot/src/main/java/com/leafBot/testcases/TC001_LoginLogout.java
+++ LeafBot/src/main/java/com/leafBot/testcases/TC001_LoginLogout.java
@@ -7,8 +7,9 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.leafBot.selenium.api.base.SeleniumBase;
+import com.leafBot.testng.api.base.ProjectSpecificMethods;
 
-public class TC001_LoginLogout extends SeleniumBase{
+public class TC001_LoginLogout extends ProjectSpecificMethods{
 	
 	@BeforeTest
 	
@@ -23,6 +24,11 @@ public class TC001_LoginLogout extends SeleniumBase{
 	
 	@Test(dataProvider="fetchData")
 	public void runTC001(String username,String password) {
+		new LoginPage()
+		.enterUsername(username)
+		.enterPassword(password)
+		.clickLogin();
+		
 		
 	}
 
